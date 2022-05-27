199ea66679b638f450b64f3d585254e437fdc580
itzish1999 <ismaeel.butt19@gmail.com>
2022-05-27 15:53:46 -0400

    feat(Manipulated Variable) created a very basic app that manipulates variables that follows the PEP 8 variable naming conventions

--- hello.py
+++ hello.py
@@ -1,3 +1,19 @@
 # This is my first ever python comment!
 """This is my first multi-line comment
 This is the beginning to understanding and using Python as a whole!"""
+
+# Working with operators / creating a variable name
+quantity = 10
+# This variable contains a float
+unit_price = 1.99
+# This variable contains the result of multiplying quantity times unit price
+extended_price = quantity * unit_price
+#Show the results
+print(extended_price)
+
+
+"""Variable naming conventions should typicall follow the PEP 8 styling conventions or Camel Case
+Python purists typically like PEP 8 because the underscore that separates multiple words are easier to distinguish
+Camel Case is also pretty common.
+(PEP 8) my_number = 2
+(Camel Case) myNumber = 2"""
