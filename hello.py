267bd6532eed1815d7a1c291d64603754b4a9f76
itzish1999 <ismaeel.butt19@gmail.com>
2022-05-29 00:28:46 -0400

    feat(Added a Boolean) Added a Boolean example that also explains the importance of indentations in Python

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 # This is my first ever python comment!
-"""This is my first multi-line comment
-This is the beginning to understanding and using Python as a whole!"""
+""" This is my first multi-line comment
+This is the beginning to understanding and using Python as a whole! """
 
 # Working with operators / creating a variable name
 quantity = 10
@@ -12,8 +12,24 @@ extended_price = quantity * unit_price
 print(extended_price)
 
 
-"""Variable naming conventions should typicall follow the PEP 8 styling conventions or Camel Case
+""" Variable naming conventions should typicall follow the PEP 8 styling conventions or Camel Case
 Python purists typically like PEP 8 because the underscore that separates multiple words are easier to distinguish
 Camel Case is also pretty common.
 (PEP 8) my_number = 2
-(Camel Case) myNumber = 2"""
+(Camel Case) myNumber = 2 """
+
+#Boolean example
+x = 10
+# If else statement depending on X
+if x == 0:
+# Really important to use : and indent the preceeding line
+    print("X is zero")
+# Same applies to the beginning of the else statement
+else:
+    print("x is ", x)
+# This code will execute no matter what BECAUSE it isn't indented
+print("All done")
+
+""" Indentation is super important in Python.
+For now its pivotal to remember that in regards to this code,
+the indented lines will execute DEPENDING on the value of x. """
