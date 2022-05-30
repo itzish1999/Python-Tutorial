acadd3ba1cd8b33db3f0dc742b531a492f327cbf
itzish1999 <ismaeel.butt19@gmail.com>
2022-05-29 23:20:06 -0400

    feat(second function example)

--- functions.py
+++ functions.py
@@ -2,7 +2,7 @@
 
 # Declaring the value of x
 x = -4
-# Creating and using the abs() function
+# Creating a var named y and using the abs() function
 y = abs(x)
 # Showing the value of x
 print(x)
@@ -12,3 +12,20 @@ print(y)
 """ If you run the code, our output should show 4 
 since abs() shows the absolute number 
 (converts negative nums to positive ones) """
+
+""" The rubric for creating functions is as follows;
+variablename = functioname(param[, param])
+The function is named and then a () is used
+you input a param and you have the option to input a second one. """
+
+# Function that uses round()
+
+# Decalaring a value for x
+x = 1.23456789098765432100000000001
+# Creating a var named y and using the round() function
+# Note that the 2 means how many decimal places
+y = round(x, 2)
+# Showing the value of x
+print(x)
+# Showing the value of y
+print(y)
