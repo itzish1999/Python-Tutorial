d6bedf6160b30c57286e74b6f9e54dcac857bab6
itzish1999 <ismaeel.butt19@gmail.com>
2022-05-29 22:54:39 -0400

    feat(Added functions file)

---
+++ functions.py
@@ -0,0 +1,14 @@
+# This file is to learn how to use functions in code.
+
+# Declaring the value of x
+x = -4
+# Creating and using the abs() function
+y = abs(x)
+# Showing the value of x
+print(x)
+# Showing the value of y
+print(y)
+
+""" If you run the code, our output should show 4 
+since abs() shows the absolute number 
+(converts negative nums to positive ones) """
