f572da7843b4b9b8f6508a3fb35b12c027821bc0
itzish1999 <ismaeel.butt19@gmail.com>
2022-06-08 22:48:02 -0400

    feat(Clarification)

--- modules.py
+++ modules.py
@@ -9,6 +9,7 @@ datetime.datetime - A single item of data consisting of date, time and optionall
 
 # Import datetime module and give it an alias of dt
 import datetime as dt
+from turtle import right
 
 # We are showing the date with the computers internal clock
 
@@ -25,8 +26,14 @@ time = dt.time(12, 35, 50, 999999)
 # datetime.time without arguments
 no_time = dt.time()
 
+# datetime.datetime outline
+# date_time = dt.datetime(year, month, day, hour, [minute, second, [microsecond]])
+
 # datetime.datetime
-date_time = dt.datetime
+date_time = dt.datetime(2020, 6, 8, 22, 45, 50)
+
+# You can even get the date / time for right now!
+right_now = dt.datetime.now()
 
 # You can use an f-string to design it as well
 print(f"{today:%A, %m/%d/%Y}")
@@ -34,3 +41,7 @@ print(no_time)
 print(manual_today)
 print(time)
 print(date_time)
+# Unedited
+# print(right_now)
+# Edited with f-string
+print( f"{right_now:%a, %b %d, %Y time %H:%M:%S}")
